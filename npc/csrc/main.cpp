c111f13e71db45269f361a9a8591c50d347eca96
x-yang03 <1979954151@qq.com>
2024-11-13 16:34:18 +0800

    verilator test

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,20 @@
 #include <stdio.h>
-
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+#include <stdlib.h>
+#include <assert.h>
+#include "Vtop.h"
+#include "verilated.h"
+int main(int argc, char** argv){
+	int time = 10;
+while (time--){
+	VerilatedContext* contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+  Vtop* top = new Vtop{contextp};
+  int a = rand() & 1;
+  int b = rand() & 1;
+  top->a = a;
+  top->b = b;
+  top->eval();
+  printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+  assert(top->f == (a ^ b));
+}
 }
