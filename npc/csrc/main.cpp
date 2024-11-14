252a0d8cf3f3d818cef931d032362da281b73079
tracer-ysyx <tracer@ysyx.org>
2024-11-14 13:59:55 +0800

    > sim RTL 2113301 xyang Linux xyang-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:59:55 up  1:30,  1 user,  load average: 0.01, 0.12, 0.17

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -3,18 +3,31 @@
 #include <assert.h>
 #include "Vtop.h"
 #include "verilated.h"
+#include "verilated_vcd_c.h"
 int main(int argc, char** argv){
 	int time = 10;
-while (time--){
 	VerilatedContext* contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
   Vtop* top = new Vtop{contextp};
-  int a = rand() & 1;
+	VerilatedVcdC *tfp= new VerilatedVcdC;   //初始化VCD对象指针
+  contextp->traceEverOn(true); //打开追踪
+	top->trace(tfp,0);
+  tfp->open("wave.vcd");  //保存位置
+
+while (time--){
+	int a = rand() & 1;
   int b = rand() & 1;
   top->a = a;
   top->b = b;
   top->eval();
   printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+	tfp->dump(contextp->time());  //dump wave
+  contextp->timeInc(1);//仿真时间推进
   assert(top->f == (a ^ b));
+
 }
+delete top;
+tfp->close();
+delete contextp;
+return 0;
 }
