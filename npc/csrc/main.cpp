f8e5dd3b04c94372d79a10ad615b6d25cfceae8d
tracer-ysyx <tracer@ysyx.org>
2024-11-20 16:33:28 +0800

    > sim RTL 2113301 xyang Linux xyang-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:33:28 up  2:32,  1 user,  load average: 0.01, 0.05, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -4,24 +4,35 @@
 #include "Vtop.h"
 #include "verilated.h"
 #include "verilated_fst_c.h"
+//#include <nvboard.h>
+
+//static TOP_NAME dut;
+
+//void nvboard_bind_all_pins(TOP_NAME* top);
+
+
 int main(int argc, char** argv){
+    // nvboard_bind_all_pins(&dut);
+    // nvboard_init();
 	int time = 10;
 	VerilatedContext* contextp = new VerilatedContext;
-  contextp->commandArgs(argc, argv);
-  Vtop* top = new Vtop{contextp};
-	VerilatedFstC *tfp= new VerilatedFstC;   //初始化VCD对象指针
-  contextp->traceEverOn(true); //打开追踪
+    contextp->commandArgs(argc, argv);
+    Vtop* top = new Vtop{contextp};
+    VerilatedFstC *tfp= new VerilatedFstC;   //初始化VCD对象指针
+    contextp->traceEverOn(true); //打开追踪
 	top->trace(tfp,0);
-  tfp->open("wave.fst");  //保存位置
+    tfp->open("wave.fst");  //保存位置
 
 while (time--){
-	int a = rand() & 1;
+  //  nvboard_update();
+  int a = rand() & 1;
   int b = rand() & 1;
   top->a = a;
   top->b = b;
   top->eval();
+  //   dut.eval();
   printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-	tfp->dump(contextp->time());  //dump wave
+  tfp->dump(contextp->time());  //dump wave
   contextp->timeInc(1);//仿真时间推进
   assert(top->f == (a ^ b));
 
