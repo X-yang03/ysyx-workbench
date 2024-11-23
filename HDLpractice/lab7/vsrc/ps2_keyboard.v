a3935a6bc30beb0040785ca9dc0d87917292f4d5
x-yang03 <1979954151@qq.com>
2024-11-23 19:40:42 +0800

    able to show cnt ascii ps2

--- HDLpractice/lab7/vsrc/ps2_keyboard.v
+++ HDLpractice/lab7/vsrc/ps2_keyboard.v
@@ -3,6 +3,7 @@ module ps2_keyboard(
     input clk,resetn,ps2_clk,ps2_data,
 //    input nextdata_n,
     output [7:0] data,
+    output reg [7:0] key_cnt,
     output reg ready,
     output reg overflow
 );
@@ -22,14 +23,14 @@ module ps2_keyboard(
 
     always @(posedge clk) begin
         if (resetn == 0) begin // reset
-            count <= 0; w_ptr <= 0; r_ptr <= 0; ready <= 0; overflow <= 0;
+            count <= 0; w_ptr <= 0; r_ptr <= 0; ready <= 0; overflow <= 0; key_cnt <= 0;
         end
         else begin
             if(ready) begin // read to output next data
                // if(nextdata_n == 0) begin
-                    r_ptr <= r_ptr + 1;
-                    if(w_ptr == (r_ptr +1)) ready <= 0;
-               // end
+                    r_ptr <= r_ptr + 3'b1;
+                    if(w_ptr == (r_ptr +3'b1)) ready <= 0;
+               // endt
             end
             if (sampling) begin
               if (count == 4'd10) begin
@@ -37,9 +38,11 @@ module ps2_keyboard(
                     (ps2_data)       &&  // stop bit
                     (^buffer[9:1])) begin      // odd  parity
                     fifo[w_ptr] <= buffer[8:1];  // store ps2_data
-                    w_ptr <= w_ptr + 1;
+                    if(buffer[8:1] == 8'hF0) key_cnt <= key_cnt +1; // loose a key
+
+                    w_ptr <= w_ptr + 3'b1;
                     ready <= 1;
-                    overflow <= overflow | ((w_ptr+1) == r_ptr);
+                    overflow <= overflow | ((w_ptr + 3'b1) == r_ptr);
                     $display("receive %x", buffer[8:1]);
                 end
                 count <= 0;     // for next
--- HDLpractice/lab7/vsrc/top.v
+++ HDLpractice/lab7/vsrc/top.v
@@ -5,10 +5,17 @@ module top(
     input rst,
     input nextdata_n,
     output [7:0] data,
+    output [7:0] ascii,
+    output [7:0] key_cnt,
+
     output ready,
     output overflow,
     output [6:0] seg_data_l,
-    output [6:0] seg_data_h
+    output [6:0] seg_data_h,
+    output [6:0] seg_ascii_l, 
+    output [6:0] seg_ascii_h,
+    output [6:0] seg_key_l,
+    output [6:0] seg_key_h
 );
     ps2_keyboard ps2_keyboard_inst(
         .clk(clk),
@@ -17,9 +24,19 @@ module top(
         .ps2_data(ps2_data),
    //     .nextdata_n(nextdata_n),
         .data(data),
+        .key_cnt(key_cnt),
         .ready(ready),
         .overflow(overflow)
     );
     seg_hex seg0(.a(data[3:0]), .h(seg_data_l));
     seg_hex seg1(.a(data[7:4]), .h(seg_data_h));
+    rom mem(
+        .addr(data),
+        .data(ascii)
+    );
+    seg_hex seg2(.a(ascii[3:0]), .h(seg_ascii_l));
+    seg_hex seg3(.a(ascii[7:4]), .h(seg_ascii_h));
+
+    seg_hex seg4(.a(key_cnt[3:0]), .h(seg_key_l)); 
+    seg_hex seg5(.a(key_cnt[7:4]), .h(seg_key_h));
 endmodule
